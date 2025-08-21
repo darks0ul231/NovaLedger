d6eddf070eea7dc6cca413726a11eef630a4790e
darks0ul231 <ceejay.angeles96@gmai.com>
2025-08-21 18:42:21 +0800

    mobile-friendly layout commit

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -3,11 +3,16 @@ import Services from "@/components/ui/homepage/Services";
 import About from "@/components/ui/homepage/About";
 import Contact from "@/components/ui/homepage/Contact";
 import Footer from "@/components/ui/homepage/Footer";
+import { Layout } from "lucide-react";
+import LayoutWrapper from "@/components/shared/LayoutWrapper";
+import Header from "@/components/ui/homepage/Header";
+
 
 export default function Home() {
   return (
-    <main className="bg-gray-50 text-gray-800">
-      <Hero />
+     <main className="bg-gray-50 text-gray-800">
+      <Header/>
+      <Hero/>
       <Services />
       <About />
       <Contact />
--- src/components/ui/homepage/Hero.tsx
+++ src/components/ui/homepage/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative bg-white">
+   <section className="relative bg-white">
       <div className="mx-auto max-w-7xl px-6 py-20 grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
         {/* Left: Text Content */}
         <div className="max-w-2xl">
@@ -29,6 +29,7 @@ export default function Hero() {
             </a>
           </div>
         </div>
+        
 
         {/* Right: Image */}
         <div className="flex justify-center lg:justify-end">
